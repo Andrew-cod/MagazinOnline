8307dcf81f8249c48b946eb0377a666824a64dbf
Andrew-cod <andrei.arapu@student.upt.ro>
2024-04-07 15:10:16 +0300

    Unit test la functiile implementate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,14 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         cumparator x = new cumparator();
         vanzator v=new vanzator();
+        admin a = new admin();
+
         System.out.println(v.creeazaCont("eu@upt.ro","jal76578Aoalo"));
         ResultSet r = conectiune.sql_get("SELECT * FROM vanzator");
         while(r.next()){
             System.out.println(r.getString("verif"));
         }
-        System.out.println(v.login("cineva@upt.ro","jal76578Aoalo"));
+        System.out.println(v.login("eu@upt.ro","jal76578Aoalo"));
 
     }
 }
